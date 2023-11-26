4d42755612d6f602d5e1822440fb77322ff773e5
Julien Roche <merlin441977@gmail.com>
2023-11-26 18:26:37 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import { useState } from 'react'
+
+//
+function App() {
+  const PRODUCTS = [
+    { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+    { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+    { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+    { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+    { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+    { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+  ]
+
+  console.log(PRODUCTS)
+
+  return (
+    <div>
+
+    </div>);
+}
+
+export default App;
+
